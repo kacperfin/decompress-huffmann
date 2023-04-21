e34ef4dd99cf02346ba347243e08fa2bd18383aa
kacperfin <kacper.aleksander.fin@gmail.com>
2023-04-21 10:21:00 +0200

    Added converting Huffmann dictionary from a file to ArrayList

---
+++ src/com/kacperfin/decompressor/Util.java
@@ -0,0 +1,12 @@
+package com.kacperfin.decompressor;
+
+import java.util.Scanner;
+
+public class BinaryFileToTxt {
+    public static int binaryFileToTxt(String input_name, String output_name)
+    {
+
+
+        return 0;
+    }
+}
